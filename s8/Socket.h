7bfa357e780bbf36da584de9a5d26ca9d22ffa0a
swings <1321457973@qq.com>
2018-08-24 13:29:27 +0000

    完成s08

---
+++ s8/Socket.h
@@ -0,0 +1,33 @@
+#ifndef __SOCKET_H__
+#define __SOCKET_H__
+
+#include <boost/noncopyable.hpp>
+
+namespace muduo {
+class InetAddress;
+
+class Socket : boost::noncopyable {
+public:
+
+    explicit Socket(int sockfd) : sockfd_(sockfd) {}
+
+    ~Socket();
+
+    int fd() const { return sockfd_; }
+
+    void bindAddress(const InetAddress& localaddr);
+
+    void listen();
+
+    int accept(InetAddress* peeraddr);
+
+    void setReuseAddr(bool on);
+
+    void shutdownWrite();
+private:
+
+    const int sockfd_;
+}; // class Socket
+} // namespace muduo
+
+#endif
---
+++ s8/TimerQueue.h
@@ -0,0 +1,54 @@
+#ifndef __TIMER_QUEUE_H__
+#define __TIMER_QUEUE_H__
+
+#include "Callbacks.h"
+#include "Channel.h"
+#include "TimerId.h"
+
+#include <boost/noncopyable.hpp>
+#include <muduo/base/Timestamp.h>
+#include <muduo/base/Mutex.h>
+
+#include <set>
+#include <vector>
+
+namespace muduo {
+class EventLoop;
+class Timer;
+// A best efforts timer queue
+// No guarantee that the callback will be on time
+class TimerQueue : boost::noncopyable {
+public:
+    TimerQueue(EventLoop* loop);
+    ~TimerQueue();
+
+    // Schedules the callback to be run at given time
+    // repeats if @c interval > 0.0
+    // Must be thread safe
+    // Usually be called from other threads
+    TimerId addTimer(const TimerCallback& cb, 
+                     Timestamp when, double interval);
+
+private:
+    // FIXME: use unique_ptr<Timer> instead of raw pointers
+    using Entry = std::pair<Timestamp, Timer*>;
+    using TimerList = std::set<Entry>;
+
+    void addTimerInLoop(Timer* timer);
+    // called when timerfd alarms
+    void handleRead();
+    // move out all expired timers
+    std::vector<Entry> getExpired(Timestamp now);
+    void reset(const std::vector<Entry>& expired, Timestamp now);
+
+    bool insert(Timer* timer);
+
+    EventLoop* loop_;
+    const int timerfd_;
+    Channel timerfdChannel_;
+    // Timer list sorted by expiration
+    TimerList timers_;
+};
+} // namespace muduo
+
+#endif
